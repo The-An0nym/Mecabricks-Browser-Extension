c735b17b9d0da5315296caf67643c72977785a44
The-An0nym <114821295+The-An0nym@users.noreply.github.com>
2025-05-18 11:46:31 +0200

    Hiding users works for a lot of content. Comments, posts, models, messages...

--- Extension/index.js
+++ Extension/index.js
@@ -1,16 +1,135 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.getElementById("del_user");
+  const hidUserToggle = document.getElementById("del_user");
+  const numNotifyToggle = document.getElementById("num_notifications");
+  const manageHidden = document.getElementById("manage-hidden");
+  const back = document.getElementById("back");
+  const hideButton = document.getElementById("hide");
 
+  /* HIDE DELETED USERS*/
   // Load saved state
-  chrome.storage.sync.get("scriptEnabled", (data) => {
-    toggle.checked = data.scriptEnabled || false;
+  chrome.storage.sync.get("hideDeletedUsers", (data) => {
+    hidUserToggle.checked = data.hideDeletedUsers || false;
   });
 
   // Listen for changes to the checkbox
-  toggle.addEventListener("change", () => {
-    const isEnabled = toggle.checked;
+  hidUserToggle.addEventListener("change", () => {
+    const isEnabled = hidUserToggle.checked;
 
     // Save the state
-    chrome.storage.sync.set({ scriptEnabled: isEnabled });
+    chrome.storage.sync.set({ hideDeletedUsers: isEnabled });
   });
+
+  /* NUMBERED NOTIFICATIONS */
+  // Load saved state
+  chrome.storage.sync.get("numberedNotifications", (data) => {
+    numNotifyToggle.checked = data.numberedNotifications || false;
+  });
+
+  // Listen for changes to the checkbox
+  numNotifyToggle.addEventListener("change", () => {
+    const isEnabled = numNotifyToggle.checked;
+
+    // Save the state
+    chrome.storage.sync.set({ numberedNotifications: isEnabled });
+  });
+
+  manageHidden.addEventListener("mouseup", listHiddenUsers);
+  back.addEventListener("mouseup", options);
+  hideButton.addEventListener("mouseup", hideUser);
 });
+
+function options() {
+  document.getElementById("options").style.display = "flex";
+  document.getElementById("hidden-users").style.display = "none";
+}
+
+function listHiddenUsers() {
+  document.getElementById("options").style.display = "none";
+  document.getElementById("hidden-users").style.display = "flex";
+  usersWrapper = document.getElementById("users-wrapper");
+  chrome.storage.sync.get("hiddenUsers", (data) => {
+    if (data.hiddenUsers) {
+      if (data.hiddenUsers.length !== 0) {
+        // Enforce that wrapper is empty
+        usersWrapper.innerHTML = "";
+        hidUsers = data.hiddenUsers;
+        for (user of hidUsers) {
+          wrapper = document.createElement("span");
+          wrapper.className = "hidden-user-wrapper item";
+
+          span = document.createElement("span");
+          span.textContent = user;
+          wrapper.appendChild(span);
+
+          del = document.createElement("span");
+          del.textContent = "🗑️";
+          del.className = "button";
+          del.addEventListener("mouseup", () => {
+            unhideUser(user);
+          });
+          wrapper.appendChild(del);
+
+          usersWrapper.appendChild(wrapper);
+        }
+      } else {
+        // Enforce that wrapper is empty
+        usersWrapper.innerHTML = "";
+        span = document.createElement("span");
+        span.textContent = "You have no hidden users...";
+        usersWrapper.appendChild(span);
+      }
+    } else {
+      // Enforce that wrapper is empty
+      usersWrapper.innerHTML = "";
+      span = document.createElement("span");
+      span.textContent = "You have no hidden users...";
+      usersWrapper.appendChild(span);
+    }
+  });
+}
+
+function hideUser() {
+  inp = document.getElementById("inp");
+  val = inp.value;
+  if (val !== "") {
+    chrome.storage.sync.get("hiddenUsers", (data) => {
+      if (data.hiddenUsers) {
+        hidUsers = data.hiddenUsers;
+        if (!hidUsers.includes(val)) {
+          // Append to list
+          hidUsers.push(val);
+          chrome.storage.sync.set({ hiddenUsers: hidUsers });
+          inp.setAttribute("placeholder", "");
+          listHiddenUsers();
+        } else {
+          inp.setAttribute("placeholder", "Already hidden");
+        }
+      } else {
+        // Create list
+        hidUsers = [];
+        hidUsers.push(val);
+        chrome.storage.sync.set({ hiddenUsers: hidUsers });
+        inp.setAttribute("placeholder", "");
+        listHiddenUsers();
+      }
+    });
+  } else {
+    inp.setAttribute("placeholder", "Invalid");
+  }
+  inp.value = "";
+}
+
+function unhideUser(user) {
+  chrome.storage.sync.get("hiddenUsers", (data) => {
+    if (data.hiddenUsers) {
+      hidUsers = data.hiddenUsers;
+      if (hidUsers.includes(user)) {
+        // Update List
+        const indx = hidUsers.indexOf(user);
+        hidUsers.splice(indx, 1);
+        chrome.storage.sync.set({ hiddenUsers: hidUsers });
+        listHiddenUsers();
+      }
+    }
+  });
+}
---
+++ Extension/removeContent.js
@@ -0,0 +1,202 @@
+// Check the toggle state and conditionally run the script
+chrome.storage.sync.get("hideDeletedUsers", (data) => {
+  if (!data.hideDeletedUsers) {
+    return;
+  }
+  if (url.includes("models")) {
+    removeComments([""]);
+  }
+  if (url.includes("topic")) {
+    removePosts([""]);
+  }
+});
+
+// Check the toggle state and conditionally run the script
+chrome.storage.sync.get("numberedNotifications", (data) => {
+  if (data.numberedNotifications) {
+    return;
+  }
+  if (url.includes("partmanager") || url.includes("notifications")) {
+    remNotifyNumber1();
+  } else {
+    remNotifyNumber2();
+  }
+});
+
+// Get data
+chrome.storage.sync.get("hiddenUsers", (data) => {
+  if (!data.hiddenUsers) {
+    return;
+  }
+  if (data.hiddenUsers.length === 0) {
+    return;
+  }
+  if (url.includes("models")) {
+    removeComments(data.hiddenUsers);
+  }
+  if (url.includes("topic")) {
+    removePosts(data.hiddenUsers);
+  }
+  if (url.includes("library")) {
+    removeModels(data.hiddenUsers);
+  }
+  if (url.includes("category")) {
+    removeThread(data.hiddenUsers);
+  }
+  if (url.includes("messages")) {
+    removeMessages(data.hiddenUsers);
+  }
+  if (url.includes("notifications")) {
+    removeNotifications(data.hiddenUsers);
+  }
+});
+
+function removeComments(users) {
+  const usernames = document.getElementsByClassName("author");
+  for (let i = usernames.length - 1; i >= 0; i--) {
+    if (users.includes(usernames[i].innerText)) {
+      usernames[i].parentNode.parentNode.parentNode.remove();
+      console.log("%c removed element!", "background: #c20; color: #fff");
+    }
+  }
+}
+
+function removePosts(users) {
+  const usernames = document.getElementsByClassName("username");
+  for (let i = usernames.length - 1; i >= 0; i--) {
+    if (users.includes(usernames[i].innerText)) {
+      usernames[i].parentNode.parentNode.parentNode.parentNode.remove();
+      console.log("%c removed element!", "background: #c20; color: #fff");
+    }
+  }
+}
+
+function removeModels(users) {
+  const usernames = document.getElementsByClassName("username");
+  for (let i = usernames.length - 1; i >= 0; i--) {
+    if (users.includes(usernames[i].innerText)) {
+      usernames[i].parentNode.parentNode.parentNode.remove();
+      console.log("%c removed element!", "background: #c20; color: #fff");
+    }
+  }
+}
+
+function removeThread(users) {
+  const usernames = document.getElementsByClassName("info");
+  for (let i = usernames.length - 1; i >= 0; i--) {
+    if (users.includes(usernames[i].getElementsByTagName("a")[0].innerText)) {
+      usernames[i].parentNode.parentNode.remove();
+      console.log("%c removed element!", "background: #c20; color: #fff");
+    }
+  }
+}
+
+function removeMessages(users) {
+  let callOnce = false;
+
+  // Select the node that will be observed for mutations
+  const targetNode = document.getElementsByClassName("nano-content")[0];
+
+  // Options for the observer (which mutations to observe)
+  const config = { childList: true };
+
+  // Callback function to execute when mutations are observed
+  const remMessages = (mutationList, observer) => {
+    if (callOnce) return;
+    callOnce = true;
+    for (const mutation of mutationList) {
+      if (mutation.type === "childList") {
+        const items = document.getElementsByClassName("item");
+        if (items.length !== 0) {
+          for (let i = items.length - 1; i >= 0; i--) {
+            if (
+              users.includes(
+                items[i].getElementsByClassName("username")[0].innerText
+              )
+            ) {
+              items[i].remove();
+            }
+          }
+        } else {
+          callOnce = false;
+        }
+      }
+    }
+  };
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(remMessages);
+
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config);
+}
+
+function removeNotifications(users) {
+  let callOnce = false;
+
+  // Select the node that will be observed for mutations
+  const targetNode = document.getElementById("notifications");
+
+  // Options for the observer (which mutations to observe)
+  const config = { childList: true };
+
+  // Callback function to execute when mutations are observed
+  const remNotifications = (mutationList, observer) => {
+    if (callOnce) return;
+    callOnce = true;
+    for (const mutation of mutationList) {
+      if (mutation.type === "childList") {
+        const items = document.getElementsByClassName("notification");
+        if (items.length !== 0) {
+          for (let i = items.length - 1; i >= 0; i--) {
+            // PRIVATE MESSAGES
+            if (
+              items[i].getElementsByClassName("title")[0].innerText ===
+              "Private conversation"
+            ) {
+              // Locate username
+              const username =
+                items[i].getElementsByClassName("user")[0].innerText;
+              if (users.includes(username)) {
+                items[i].remove();
+              }
+              // MODELS
+            } else if (items[i].getElementsByClassName("image-container")) {
+              // Locate username
+              username = items[i].getElementsByClassName("user")[0].innerText;
+              if (users.includes(username)) {
+                items[i].remove();
+              }
+            }
+          }
+        } else {
+          callOnce = false;
+        }
+      }
+    }
+  };
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(remNotifications);
+
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config);
+}
+
+function remNotifyNumber1() {
+  const u = document.getElementsByClassName("user")[0];
+  if (u.getElementsByClassName("notifications")) {
+    document.getElementsByClassName("notifications")[0].innerText = "";
+  }
+}
+
+function remNotifyNumber2() {
+  if (!document.getElementById("header-notifications")) return;
+  badge = document.getElementById("header-notifications");
+  badge.innerText = "";
+  badge.style.padding = "0";
+  badge.style.width = "16px";
+  badge.style.height = "16px";
+  badge.style.marginLeft = "-2px";
+  badge.style.borderRadius = "50%";
+}
